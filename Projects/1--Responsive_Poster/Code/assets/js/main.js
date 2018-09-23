055305e2de928ac6d855f0ec39ec29cf89b32f71
Devin Halladay <devin@devinhalladay.com>
2018-09-23 00:03:53 -0400

    finished responsiveness

--- Projects/1--Responsive_Poster/Code/assets/js/main.js
+++ Projects/1--Responsive_Poster/Code/assets/js/main.js
@@ -1,17 +1,56 @@
-var arrowLine = document.querySelector("#arrow-line");
-var arrowChevron = document.querySelector("#arrow-chevron");
-var arrowContainer = document.querySelector("#cta");
+var ctaArrowLine = document.querySelector("#cta-arrow-line");
+var ctaArrowChevron = document.querySelector("#cta-arrow-chevron");
+var ctaArrowContainer = document.querySelector("#cta");
 
-arrowContainer.onmouseover = function () {
-  arrowChevron.classList.remove("anim-chevron--out");
-  arrowLine.classList.remove("anim-line--out");
-  arrowChevron.classList.add("anim-chevron--in");
-  arrowLine.classList.add("anim-line--in");
+var taglineArrowLine = document.querySelector("#tagline-arrow-line");
+var taglineArrowChevron = document.querySelector("#tagline-arrow-chevron");
+var taglineArrowContainer = document.querySelector("#tagline-cta");
+
+var tinyTaglineArrowLine = document.querySelector("#tiny-tagline-arrow-line");
+var tinyTaglineArrowChevron = document.querySelector("#tiny-tagline-arrow-chevron");
+var tinyTaglineArrowContainer = document.querySelector("#tiny-tagline-cta");
+
+// CTA arrow
+ctaArrowContainer.onmouseover = function () {
+  ctaArrowChevron.classList.remove("anim-chevron--out");
+  ctaArrowLine.classList.remove("anim-line--out");
+  ctaArrowChevron.classList.add("anim-chevron--in");
+  ctaArrowLine.classList.add("anim-line--in");
 };
 
-arrowContainer.onmouseout = function () {
-  arrowChevron.classList.remove("anim-chevron--in");
-  arrowLine.classList.remove("anim-line--in");
-  arrowChevron.classList.add("anim-chevron--out");
-  arrowLine.classList.add("anim-line--out");
+ctaArrowContainer.onmouseout = function () {
+  ctaArrowChevron.classList.remove("anim-chevron--in");
+  ctaArrowLine.classList.remove("anim-line--in");
+  ctaArrowChevron.classList.add("anim-chevron--out");
+  ctaArrowLine.classList.add("anim-line--out");
+};
+
+//Tagline Arrow
+taglineArrowContainer.onmouseover = function () {
+  taglineArrowChevron.classList.remove("anim-chevron--out");
+  taglineArrowLine.classList.remove("anim-line--out");
+  taglineArrowChevron.classList.add("anim-chevron--in");
+  taglineArrowLine.classList.add("anim-line--in");
+};
+
+taglineArrowContainer.onmouseout = function () {
+  taglineArrowChevron.classList.remove("anim-chevron--in");
+  taglineArrowLine.classList.remove("anim-line--in");
+  taglineArrowChevron.classList.add("anim-chevron--out");
+  taglineArrowLine.classList.add("anim-line--out");
+};
+
+//Tiny Tagline Arrow
+tinyTaglineArrowContainer.onmouseover = function () {
+  tinyTaglineArrowChevron.classList.remove("anim-chevron--out");
+  tinyTaglineArrowLine.classList.remove("anim-line--out");
+  tinyTaglineArrowChevron.classList.add("anim-chevron--in");
+  tinyTaglineArrowLine.classList.add("anim-line--in");
+};
+
+tinyTaglineArrowContainer.onmouseout = function () {
+  tinyTaglineArrowChevron.classList.remove("anim-chevron--in");
+  tinyTaglineArrowLine.classList.remove("anim-line--in");
+  tinyTaglineArrowChevron.classList.add("anim-chevron--out");
+  tinyTaglineArrowLine.classList.add("anim-line--out");
 };
