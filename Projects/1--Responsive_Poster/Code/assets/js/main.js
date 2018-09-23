1664356f14a7d81ca3a3953ee28b25bf502f955c
Devin Halladay <devin@devinhalladay.com>
2018-09-22 22:53:06 -0400

    control css animation with js

---
+++ Projects/1--Responsive_Poster/Code/assets/js/main.js
@@ -0,0 +1,18 @@
+// Get references to elements and controlling buttons
+var arrowLine = document.querySelector("#arrow-line");
+var arrowChevron = document.querySelector("#arrow-chevron");
+var arrowContainer = document.querySelector("#cta");
+
+arrowContainer.onmouseover = function () {
+  arrowChevron.classList.remove("anim-chevron--out");
+  arrowLine.classList.remove("anim-line--out");
+  arrowChevron.classList.add("anim-chevron--in");
+  arrowLine.classList.add("anim-line--in");
+};
+
+arrowContainer.onmouseout = function () {
+  arrowChevron.classList.remove("anim-chevron--in");
+  arrowLine.classList.remove("anim-line--in");
+  arrowChevron.classList.add("anim-chevron--out");
+  arrowLine.classList.add("anim-line--out");
+};
